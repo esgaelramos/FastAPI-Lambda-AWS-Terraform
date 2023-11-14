7202f8cdae0c472517d449f752df2840186e45a5
Gael Ramos <esgaelramos@gmail.com>
2023-11-14 01:18:11 -0600

    add draft in main.tf for move backend to s3

--- infra-lambda/main.tf
+++ infra-lambda/main.tf
@@ -3,6 +3,12 @@ terraform {
   required_providers {
     aws = { source = "hashicorp/aws", version = "5.17.0" }
   }
+
+  backend "s3" {
+    bucket = "lambda-api-terraform-state-bucket"
+    key    = "estado/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
